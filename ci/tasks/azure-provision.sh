38f48d9706f8beb9b5baaf15212d32c2ffd11018
Bin Xia <binxi@microsoft.com>
2016-05-15 07:13:51 +0000

    Create a separate resource group for the network

--- ci/tasks/azure-provision.sh
+++ ci/tasks/azure-provision.sh
@@ -8,6 +8,7 @@ check_param AZURE_CLIENT_ID
 check_param AZURE_CLIENT_SECRET
 check_param AZURE_TENANT_ID
 check_param AZURE_GROUP_NAME
+check_param AZURE_GROUP_NAME_2
 check_param AZURE_REGION_NAME
 check_param AZURE_REGION_SHORT_NAME
 check_param AZURE_STORAGE_ACCOUNT_NAME
@@ -43,7 +44,7 @@ set -e
 
 echo azure group create ${AZURE_GROUP_NAME} ${AZURE_REGION_SHORT_NAME}
 azure group create ${AZURE_GROUP_NAME} ${AZURE_REGION_SHORT_NAME}
-cat > provision-parameters.json << EndOfMessage
+cat > provision-parameters.json << EOF
 {
   "newStorageAccountName": {
     "value": "${AZURE_STORAGE_ACCOUNT_NAME}"
@@ -61,10 +62,52 @@ cat > provision-parameters.json << EndOfMessage
     "value": "${AZURE_CF_SUBNET_NAME}"
   }
 }
-EndOfMessage
+EOF
 azure group deployment create ${AZURE_GROUP_NAME} --template-file ./bosh-cpi-release/ci/assets/azure/provision.json --parameters-file ./provision-parameters.json
 
 # Setup the storage account
 AZURE_ACCOUNT_KEY=$(azure storage account keys list ${AZURE_STORAGE_ACCOUNT_NAME} --resource-group ${AZURE_GROUP_NAME} --json | jq '.storageAccountKeys.key1' -r)
 azure storage container create --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --account-key ${AZURE_ACCOUNT_KEY} --container bosh
 azure storage container create --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --account-key ${AZURE_ACCOUNT_KEY} --permission blob --container stemcell
+
+set +e
+
+# Check if the resource group already exists
+echo "azure group list | grep ${AZURE_GROUP_NAME_2}"
+azure group list | grep ${AZURE_GROUP_NAME_2}
+
+if [ $? -eq 0 ]
+then
+  echo "azure group delete ${AZURE_GROUP_NAME_2}"
+  azure group delete ${AZURE_GROUP_NAME_2} --quiet
+  echo "waiting for delete operation to finish..."
+  # Wait for the completion of deleting the resource group
+  azure group show ${AZURE_GROUP_NAME_2}
+  while [ $? -eq 0 ]
+  do
+    azure group show ${AZURE_GROUP_NAME_2} > /dev/null 2>&1
+    echo "..."
+  done
+fi
+
+set -e
+
+echo azure group create ${AZURE_GROUP_NAME_2} ${AZURE_REGION_SHORT_NAME}
+azure group create ${AZURE_GROUP_NAME_2} ${AZURE_REGION_SHORT_NAME}
+cat > network-parameters.json << EOF
+{
+  "virtualNetworkNameForBats": {
+    "value": "${AZURE_VNET_NAME_FOR_BATS}"
+  },
+  "virtualNetworkNameForLifecycle": {
+    "value": "${AZURE_VNET_NAME_FOR_LIFECYCLE}"
+  },
+  "subnetNameForBosh": {
+    "value": "${AZURE_BOSH_SUBNET_NAME}"
+  },
+  "subnetNameForCloudFoundry": {
+    "value": "${AZURE_CF_SUBNET_NAME}"
+  }
+}
+EOF
+azure group deployment create ${AZURE_GROUP_NAME_2} --template-file ./bosh-cpi-release/ci/assets/azure/network.json --parameters-file ./network-parameters.json
