c39d491570e100ee96cbb12e5066da2201c25091
Bin Xia <binxi@microsoft.com>
2017-12-04 15:37:19 +0000

    To remove

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -9,12 +9,6 @@ chruby ${RUBY_VERSION}
 semver=`cat version-semver/number`
 
 pushd bosh-cpi-src > /dev/null
-  echo "running unit tests"
-  pushd src/bosh_azure_cpi > /dev/null
-    bundle install
-    bundle exec rspec spec/unit/*
-  popd > /dev/null
-
   cpi_release_name="bosh-azure-cpi"
 
   echo "building CPI release..."
